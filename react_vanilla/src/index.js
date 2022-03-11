902bb85a34551728480e49fb301c317c20afd039
keinn51 <soogood97@gmail.com>
2022-03-11 16:05:49 +0900

    END #2.3 Delete To Do part two

--- react_vanilla/src/index.js
+++ react_vanilla/src/index.js
@@ -25,7 +25,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -33,14 +33,14 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-// store.subscribe(() => console.log(store.getState()));
+store.subscribe(() => console.log(store.getState()));
 
 const dispatchAddToDo = (text) => {
   store.dispatch(addToDo(text));
 };
 
 const dispatchDeleteToDo = (e) => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
