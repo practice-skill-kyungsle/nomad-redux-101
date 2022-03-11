588f970c518f557f06dbfdde230ee451bb36d524
keinn51 <soogood97@gmail.com>
2022-03-12 01:15:17 +0900

    END #4.2 createReducer

--- react_redux/src/store.js
+++ react_redux/src/store.js
@@ -1,32 +1,28 @@
 import { createStore } from "redux";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const reducer = (state = [], action) => {
+//   console.log(action);
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
